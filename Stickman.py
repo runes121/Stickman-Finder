0adc05cc197324d1840d0c480a231851e0a84ba9
runes121 <75335579+runes121@users.noreply.github.com>
2023-04-16 11:40:49 +0100

    Update Stickman.py

--- Stickman.py
+++ Stickman.py
@@ -8,8 +8,8 @@ import win32con
 
 while True:
     if pyautogui.locateOnScreen("sticky.png", confidence=0.8) != None:
-        print("Sticky!!!! :))))")
+        print("Stickman!!")
         time.sleep(0.5)
     else:
-        print("No sticky :(")
+        print("No stickman.")
         time.sleep(0.5)
